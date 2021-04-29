2763d404361ee89cbfbe694d15ed04522587a18f
ezinber <ezinber@yandex.ru>
2021-04-30 00:16:47 +0300

    feat: базовый интерфейс таск-листа

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from 'react';
 import SettingsContext from '../contexts/settingsContext';
 import Timer from './Timer/Timer';
+import Task from './Task/Task';
 import useLocalStorage from '../hooks/useLocalStorage';
 import './app.css';
 
 function App() {
   const defaultSettings = useContext(SettingsContext);
   const [settings, setSetting] = useLocalStorage('settings', defaultSettings);
+  const [tasks, setTasks] = React.useState(defaultTasks); //(Task)
 
   const handleTimerEnd = (mode) => {
     console.log('Time in mode %d is out', mode);
@@ -22,6 +24,29 @@ function App() {
 
   const handleTimerModeChange = (mode) => {
     console.log('Timer mode was changed on:', mode);
+  }
+
+
+
+  function handleTaskDelete(task) {
+    setTasks(state => state.filter(current => current.id !== task.id));
+  }
+
+  function handleTaskComplete(task) {
+    task.isCompleted = !task.isCompleted;
+    task.isActive && (task.isActive = !task.isActive);
+    setTasks(state => state.map(current => current.id === task.id ? task : current))
+  }
+
+  function handleTaskClick(task) {
+    console.log('click');
+    setTasks(state => state.map(current => {
+      current.id !== task.id
+      ? current.isActive = false
+      : current.isActive = true;
+
+      return current;
+    }));
   }
 
   /*Так как немного изменилась структура объекта настроек,
@@ -44,9 +69,43 @@ function App() {
           onEnd={handleTimerEnd}
           onModeChange={handleTimerModeChange}
         />
+
+        {/* Список задач возможно стоит вынести в отдельный компонент (Task)*/}
+        <ul className="tasklist">
+          {tasks.map(taskData => (
+            <Task
+              task={taskData}
+              onDelete={handleTaskDelete}
+              onComplete={handleTaskComplete}
+              onClick={handleTaskClick}
+            />
+          ))}
+        </ul>
+
       </div>
     </SettingsContext.Provider>
   );
 }
+
+let defaultTasks = [ // массив задач для тестов (Task)
+  {
+    id: '1',
+    text: 'first',
+    isActive: true,
+    isCompleted: false
+  },
+  {
+    id: '2',
+    text: 'second',
+    isActive: false,
+    isCompleted: false
+  },
+  {
+    id: '3',
+    text: 'third',
+    isActive: false,
+    isCompleted: true
+  },
+]
 
 export default App;
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import './Task.css';
 
-function Task ({ id, isActive, isCompleted, onComplete, onDelete }) {
+function Task ({ task, onComplete, onDelete, onClick }) {
+  const [text, setText] = React.useState(task.text);
 
-  const [text, setText] = React.useState('test');
   const taskClassName = (
-    `task ${isActive ? 'task_active' : ''} ${isCompleted ? 'task_completed' : ''}`
+    `task${task.isActive ? ' task_active' : ''}${task.isCompleted ? ' task_completed' : ''}`
+  )
+  const completeButtonClassName = (
+    `task-button task__complete-button${task.isCompleted ? ' task__complete-button_checked' : ''}`
   )
 
   function handleChange(e) {
     setText(e.target.value);
   }
 
+  function handleDelete() {
+    onDelete(task);
+  }
+
+  function handleComplete() {
+    onComplete(task);
+  }
+
+  function handleClick() {
+    !task.isCompleted && !task.isActive && onClick(task);
+  }
+
   return (
-    <div className={taskClassName}>
-      <input className="task__text" onChange={handleChange}>{text}</input>
-    </div>
+    <li className={taskClassName} key={task.id}>
+      <button type="button" className={completeButtonClassName} onClick={handleComplete} title={task.isCompleted ? 'uncheck' : 'check'} />
+      <input type="text" className="task__text" onClick={handleClick} onChange={handleChange} value={text} disabled={task.isCompleted} />
+      <button type="button" className="task-button task__delete-button" onClick={handleDelete} title="удалить" />
+    </li>
   )
 }
 
